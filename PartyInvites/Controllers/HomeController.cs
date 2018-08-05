e0f58d5be17ef8f70bafb56c82fd7b3973b0677e
Gregory Petrukhin <parentalbox@gmail.com>
2018-08-06 01:52:31 +0300

    added simple dynamic output

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace PartyInvites.Controllers
         // GET: Home
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good morning" : "good afternoon";
             return View();
         }
     }
