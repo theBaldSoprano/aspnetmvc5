19d19a7221cc9da437562307d72ccbf5ba338bb4
Gregory Petrukhin <parentalbox@gmail.com>
2018-08-09 23:07:44 +0300

    added project for auto test study

---
+++ StudyAuto/EntryPoint/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyAuto.EntryPoint {
+    class EntryPoint {
+        public static void Main(String[] args) {
+            String[] lines = { "foo", "bar", "baz" };
+            System.Console.WriteLine(lines[0]);
+
+        }
+    }
+}
