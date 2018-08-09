7a37b11a37f5c1faaa695ae0a81de8f5fa86e3ba
Gregory Petrukhin <parentalbox@gmail.com>
2018-08-10 00:38:29 +0300

    added gettersetter samples

--- StudyAuto/EntryPoint/EntryPoint.cs
+++ StudyAuto/EntryPoint/EntryPoint.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudyAuto.GetterSetter;
 
 namespace StudyAuto.EntryPoint {
     class EntryPoint {
         public static void Main(String[] args) {
             String[] lines = { "foo", "bar", "baz" };
-            System.Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[0]);
+            Console.Out.WriteLine("");
+  
+            Console.BackgroundColor = ConsoleColor.Green;
 
+            Person p = new Person();
+            p.Name = "gregory";
+            Console.WriteLine(p.Name);
+
+            Console.Beep();
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ StudyAuto/GetterSetter/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyAuto.GetterSetter {
+    class Person {
+        //variations of getters and setters
+        private string _name;
+
+        public string getName() {
+            Console.Write("do something");
+            return this._name;
+        }
+
+        public string Name {
+            get {
+                Console.WriteLine("im in getter");
+                return this._name;
+            }
+            set {
+                Console.WriteLine("im in SETTER");
+                this._name = value;
+            }
+        }
+
+        //just plain ol gs/etters
+        public string Sirname { set; get; }
+    }
+}
