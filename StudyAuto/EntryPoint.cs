a7422acdc3427da7281431119ae58d52920c1d26
Gregory Petrukhin <parentalbox@gmail.com>
2018-08-10 01:58:57 +0300

    added some inheritance samples

--- StudyAuto/EntryPoint.cs
+++ StudyAuto/EntryPoint.cs
@@ -9,18 +9,21 @@ using StudyAuto.MyString;
 namespace StudyAuto {
     class EntryPoint {
         public static void Main(String[] args) {
-            String[] lines = { "foo", "bar", "baz" };
-            Console.WriteLine(lines[0]);
-            Console.Out.WriteLine("");
-            Console.BackgroundColor = ConsoleColor.Green;
-            Person p = new Person();
-            p.Name = "gregory";
-            Console.WriteLine(p.Name);
-   
-            Console.ReadKey();
-            Word dic = new Word("foo");
-            Console.ReadKey();
-            Console.Write(dic.word);
+            {
+                String[] lines = { "foo", "bar", "baz" };
+                Console.WriteLine(lines[0]);
+                Console.Out.WriteLine("");
+                Console.BackgroundColor = ConsoleColor.Green;
+                Person p = new Person();
+                p.Name = "gregory";
+                Console.WriteLine(p.Name);
+
+                Console.ReadKey();
+                Word dic = new Word("foo");
+                Console.ReadKey();
+                Console.Write(dic.word);
+            }
+
 
 
             Console.ReadKey();
---
+++ StudyAuto/Inheritance/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyAuto.Inheritance {
+    public abstract class Animal {
+
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Animal(string name, int x, int y) {
+            this.Name = name;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public abstract Animal Reproduce();
+
+        public abstract void MoveTo(int x, int y);
+
+        public void Die(string deathcry) {
+            Console.Write(deathcry);
+            X = -1;
+            Y = -1;
+        }
+    }
+
+    //EXTENDS
+    public class Fish : Animal {
+
+        public ConsoleColor Flippers { get; set; }
+
+        //HERE CALL TO PARENT CONSTRUCTOR
+        public Fish(string name, int x, int y, ConsoleColor flippersColor) : base(name, x, y){
+            this.Flippers = flippersColor;
+        }
+
+        public override void MoveTo(int x, int y) {
+            this.Swim(x, y);
+        }
+
+        //OVERRIDING
+        public override Animal Reproduce() {
+            return new Fish(
+                this.Name + "_child",
+                this.X,
+                this.Y,
+                this.Flippers
+            );
+        }
+
+        private void Swim(int x, int y) {
+            Console.Write(String.Format("fish is swimming to %d, %d", x, y));
+        }
+    }
+}
